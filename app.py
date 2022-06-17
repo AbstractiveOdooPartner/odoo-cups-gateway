36f68b18acfe0cfda388ee7f058f4b0f1cd25bbf
vincent baggerman <vincent@abstractive.be>
2022-06-17 13:14:51 +0200

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 import cups
 import time
 import datetime
@@ -8,25 +9,25 @@ from odoorpc import ODOO
 # PARAMETERS #
 # ---------- #
 
-ODOO_HOST = ""
-ODOO_PORT = "443"
-ODOO_PROTOCOL = "jsonrpc+ssl"
-ODOO_DB = ""
-ODOO_USER = ""
-ODOO_PASSWORD = ""
-DEFAULT_PRINTER = ""
+ODOO_HOST = "mydb.odoo.com"
+ODOO_PORT = "443" # can remain unchanged
+ODOO_PROTOCOL = "jsonrpc+ssl" #can remain unchanged
+ODOO_DB = "DATABASE"
+ODOO_USER = "admin"
+ODOO_PASSWORD = "adminpassword"
+DEFAULT_PRINTER = "PRINTER_NAME"
 
 # ----------- #
 # APPLICATION #
 # ----------- #
 
 if __name__ == "__main__":
-    logger.add(f"/opt/odoo_cups_gw/logs/{datetime.date.today().isoformat()}.log", rotation="00:00")
+    logger.add(f"/opt/odoo-cups-gateway/logs/{datetime.date.today().isoformat()}.log", rotation="00:00")
     logger.info(f"Startup")
     odoo = ODOO(ODOO_HOST, ODOO_PROTOCOL, ODOO_PORT)
     odoo.login(ODOO_DB, ODOO_USER, ODOO_PASSWORD)
     connection = cups.Connection()
-    jobs_waiting = odoo.env["printer.queue"].search([["printed_by", "in", [False, ""]]])
+    jobs_waiting = odoo.env["printer.queue"].search([["printed_on", "!=", False]])
     logger.info(f"Jobs waiting: {jobs_waiting}")
 
     while True:
@@ -48,14 +49,22 @@ if __name__ == "__main__":
         logger.info(f"Job ID's received: {job_ids}")
         jobs = odoo.env["printer.queue"].browse(job_ids)
         for job in jobs:
+            filename = False
             if job.raw_report:
-                filename = f"/opt/odoo_cups_gw/labels/{job.id}.txt"
+                filename = f"/opt/odoo-cups-gateway/raw/{job.id}.txt"
                 with open(filename, mode="w") as file:
                     file.write(job.raw_report)
+            elif job.pdf_report:
+                filename = f"/opt/odoo-cups-gateway/pdf/{job.id}.pdf"
+                report = job.pdf_report.encode("utf-8")
+                with open(filename, 'wb') as file:
+                    fileno = file.write(base64.decodebytes(report))
+            if filename:
                 connection.printFile(
-                    DEFAULT_PRINTER, filename, filename, options={"raw": "1"}
+                    job.preferred_printer or DEFAULT_PRINTER, filename, filename, options={"raw": "1"} if job.raw_report else {}
                 )
-                logger.info(f"Printed Job: {job.id}")
-            job.printed_by = DEFAULT_PRINTER
+
+            logger.info(f"Printed Job: {job.id}")
             job.printed_on = datetime.date.today()
+
             odoo.env.commit()
